b90d7a1a395b60bc896665a2bf2996d450ffa65f
Justin Rovang <justin.rovang@minnesota.edu>
2015-02-19 14:33:07 -0600

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
-# carbon-fy
-A carbon extension for determining fiscal year of a date
+# Carbon Extension: Fiscal Year (FY)
+
+[![Build Status](https://travis-ci.org/rovangju/carbon-fy.svg?branch=master)](https://travis-ci.org/rovangju/carbon-fy)
+
+-------
+
+Don't reinvent the wheel on that funky date logic to determine what fiscal year it is, or what fiscal year something happened in. 
+This simple package has your back!
+
+
+## Overview
+The ideal usage scenario would be to use a single instance of the calculator, as most organizations observe a single FY 
+for their accounting.
+
+Usage is pretty straight forward:
+
+### Basic usage ###
+```php
+
+use CarbonExt\FiscalYear\Calculator;
+
+$c = new Calculator(7, 1); /* FY starts on July 1 */
+
+$c->get(new Carbon('2015-01-01')); /* 2015-06-29 */
+$c->get(new Carbon('2015-07-01')); /* 2016-06-29 */
+
+```
---
+++ src/CarbonExt/FiscalYear/Calculator.php
@@ -0,0 +1,59 @@
+<?php
+/**
+ * Carbon Extension: Fiscal Year (FY)
+ * @license GPLv2
+ * @author Justin Rovang <generate@itnobody.com>
+ */
+
+namespace CarbonExt\FiscalYear;
+
+use Carbon\Carbon;
+
+class Calculator {
+
+    public $month;
+    public $day;
+
+    /**
+     * @param int $m FY Start month
+     * @param int $d FY start day
+     */
+    public function __construct($m = 1, $d = 1) {
+        $this->month = (int)$m;
+        $this->day = (int)$d;
+    }
+
+    /**
+     * Get the FY end date
+     *
+     * @param Carbon $dt Date to determine FY for
+     *
+     * @return Carbon Carbon instance set to the end of the FY for the input
+     */
+    public function get(Carbon $dt = NULL) {
+
+        if (!$dt) {
+            $dt = new Carbon();
+        }
+
+        /* Disregard times */
+        $dt->setTime(0, 0, 0);
+
+        /* FY is based on the -end- of the FY, thus we will work backward to determine if we need to 'rollup' the FY
+        from the input year */
+
+        $fyStart = Carbon::create($dt->year, $this->month, $this->day, 0, 0, 0);
+
+        $fyEnd = clone $fyStart;
+        $fyEnd->addDays(364); /* Set to last date */
+
+        if ($dt->between($fyStart, $fyEnd, TRUE)) {
+            $FY = $fyEnd->year;
+        } else {
+            $fyEnd->year($dt->year);
+            $FY = $dt->year;
+        }
+
+        return Carbon::create($FY, $this->month, $this->day);
+    }
+}
